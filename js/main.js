3d86a76c04eecc71b007d21176d543e29f702ce5
SarvarRahim <tesla_19@mail.ru>
2021-09-24 21:42:53 +0500

    add search of language

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const country = [];
+const countrys = [];
 
 // SEARCH FORM
 const elFormSearch = document.querySelector('.form-search');
 const elSearchInput = elFormSearch.querySelector('.book-search');
 const elSelect = elFormSearch.querySelector('select');
 const elSortSelect = elFormSearch.querySelector('.js-sort-select');
+const elFormStartYear = elFormSearch.querySelector('.js-start-year');
+const elFormEndYear = elFormSearch.querySelector('.js-start-end');
+const elFormLanguage = elFormSearch.querySelector('.js-language-input');
 
 // Books List
 const elBooksList = document.querySelector('.books-list');
@@ -12,7 +15,42 @@ const elBooksList = document.querySelector('.books-list');
 const elBooksListTemplate = document.querySelector('#Books-list-templete').content;
 
 // FUNCTIONS
+// function getCountrys() {
+//   const cont = [];
+//   books.forEach(book => {
+//     cont.push(book.country)
+//     cont.forEach(count => {
+//       if(!book.includes(count)){
+//         countrys.push(cont)
+//     }
+//   }
+// }a
+// });
 
+function getCountrys (){
+  const cont = [];
+  books.forEach(book => {
+    cont.push(book.country);
+    cont.forEach(count => {
+      if (!countrys.includes(count)){
+        countrys.push(count);
+      }
+    });
+  });
+  countrys.sort();
+}
+
+function showCountry() {
+  const elCounutryFragment = document.createDocumentFragment();
+  countrys.forEach(countr => {
+    const elCountrOption = document.createElement('option');
+    elCountrOption.textContent = countr;
+    elCountrOption.value = countr;
+    elCounutryFragment.appendChild(elCountrOption);
+  });
+  elSelect.appendChild(elCounutryFragment);
+  console.log(elSelect);
+}
 
 function showBooks (books, titleRegex = '') {
   elBooksList.innerHTML = '';
@@ -27,8 +65,8 @@ function showBooks (books, titleRegex = '') {
     }else {
       elNewBookItem.querySelector('.book-title').textContent = book.title
     }
-    // elNewBookItem.querySelector('.book-title').textContent = book.title
     elNewBookItem.querySelector('.book-author').textContent = book.author;
+    elNewBookItem.querySelector('.book-country').textContent = book.country;
     elNewBookItem.querySelector('.book-lenguage').textContent = book.language;
     elNewBookItem.querySelector('.book-year').textContent = +book.year;
     elNewBookItem.querySelector('.book-pages').textContent = +book.pages;
@@ -39,35 +77,42 @@ function showBooks (books, titleRegex = '') {
   elBooksList.appendChild(elBooksFrargment);
 }
 
-function findBook(titleRegex) {
+function findBook(titleRegex, languageRegex) {
   return books.filter(book => {
-    const criteir = book.title.match(titleRegex);
+    const criteir = book.title.match(titleRegex) && (elSelect.value === 'All' || book.country.includes(elSelect.value)) && (elFormStartYear.value.trim() === '' || book.year >= Number(elFormStartYear.value)) && (elFormEndYear.value.trim() === '' || book.year <= Number(elFormEndYear.value)) && book.language.match(languageRegex);
     return criteir;
   });
 }
 
-// function sortBooks(books, sortType) {
-//   if (sortType === 'az') {
-//     books.sort((a, b) => {
-//       if (a.title > b.title) return 1;
-//       if (a.title < b.title) return 0;
-//     });
-//   } else if (sortType === 'za') {
-//     books.sort((a,b) => {
-//       if (a.title < b.title) return 1;
-//       if (a.title > b.title) return 0;
-//     });
-//   }
-// }
+function sortBooks(books, sortType) {
+  if (sortType === 'az') {
+    books.sort((a, b) => {
+      if (a.title > b.title) return 1;
+      if (a.title < b.title) return -1;
+      return 0;
+    });
+  } else if (sortType === 'za') {
+    books.sort((a,b) => {
+      if (a.title < b.title) return 1;
+      if (a.title > b.title) return -1;
+      return 0
+    });
+  } else if (sortType === 'year_new') {
+    books.sort((a, b) => a.year - b.year);
+  }else if (sortType === 'year_old') {
+    books.sort((a, b) => b.year - a.year);
+}
+}
 
 function bookFind (evt) {
   evt.preventDefault();
 
   const titleRegex = new RegExp (elSearchInput.value.trim(), 'gi');
-  const foundBooks = findBook(titleRegex);
+  const languageRegex = new RegExp (elFormLanguage.value.trim(), 'gi');
+  const foundBooks = findBook(titleRegex, languageRegex);
   if(foundBooks.length > 0) {
-    // sortBooks(foundBooks, elSortSelect.value);
-    showBooks(foundBooks, titleRegex);
+    sortBooks(foundBooks, elSortSelect.value);
+    showBooks(foundBooks, titleRegex, languageRegex);
   }else {
     elBooksList.innerHTML = '<div class="col-12">No book found</div>';
   }
@@ -77,5 +122,8 @@ if (elFormSearch) {
   elFormSearch.addEventListener('submit', bookFind)
 }
 
+
+getCountrys();
+showCountry();
 showBooks(books.slice(0, 50));
 
